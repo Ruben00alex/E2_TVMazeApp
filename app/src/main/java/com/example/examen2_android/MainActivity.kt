a06ce41d12dc1a8ffeaaaa52dee3bd46fcbacde5
Alex <ruben.ramos51@uabc.edu.mx>
2024-06-07 03:39:23 -0700

    landing page

--- app/src/main/java/com/example/examen2_android/MainActivity.kt
+++ app/src/main/java/com/example/examen2_android/MainActivity.kt
@@ -5,11 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -17,21 +20,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.room.Room
-import com.example.examen2_android.data.db.AppDatabase
-import com.example.examen2_android.data.repository.ShowRepository
 import com.example.examen2_android.ui.theme.Examen2_AndroidTheme
-import com.example.examen2_android.ui.view.screens.MainScreen
-import com.example.examen2_android.viewmodel.ShowViewModel
-import com.example.examen2_android.viewmodel.ShowViewModelFactory
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -47,14 +43,13 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {GoToDashboardButton()
-//                    MainScreen(showList = showViewModel.shows.collectAsState().value)
+                ) {
+                    LandingPage()
                 }
             }
         }
     }
     @Composable
-    ////Button with implicit intent to open DashboardActivity
     fun GoToDashboardButton() {
         Button(onClick = {
             val intent = Intent(this, DashboardActivity::class.java)
@@ -68,6 +63,47 @@ class MainActivity : ComponentActivity() {
             shape = RoundedCornerShape(16.dp)
         ) {
             Text("Go to Dashboard")
+        }
+    }
+
+
+    @Composable
+    fun LandingPage(modifier: Modifier = Modifier) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.tvmaze),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.matchParentSize()
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp)
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "TVMaze App",
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Discover your favorite TV shows!",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+                GoToDashboardButton()
+            }
         }
     }
 }
