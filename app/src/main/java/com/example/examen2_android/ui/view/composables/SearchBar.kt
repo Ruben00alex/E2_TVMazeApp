cc8936e8517ca4574a2eec2c83ebbd72a1d3a47f
Alex <ruben.ramos51@uabc.edu.mx>
2024-06-07 03:06:47 -0700

    misc

--- app/src/main/java/com/example/examen2_android/ui/view/composables/SearchBar.kt
+++ app/src/main/java/com/example/examen2_android/ui/view/composables/SearchBar.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
