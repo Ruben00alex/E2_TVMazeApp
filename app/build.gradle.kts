423e58b93adca91710d654decb173bef464090c7
Alex <ruben.ramos51@uabc.edu.mx>
2024-06-07 01:45:41 -0700

    misc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ android {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("com.google.android.material:material:1.12.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     // Room
@@ -72,7 +73,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-
+    implementation ("androidx.compose.ui:ui-text:1.6.7")
     //coil for image loading
     implementation("io.coil-kt:coil:2.6.0")
     implementation("androidx.core:core-ktx:1.13.1")
