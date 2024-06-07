2c2e7fece8be96c540f08b92a191c22ea422a2c8
Alex <ruben.ramos51@uabc.edu.mx>
2024-06-07 03:07:01 -0700

    Added image to search results

--- app/src/main/java/com/example/examen2_android/ui/view/composables/SearchListItem.kt
+++ app/src/main/java/com/example/examen2_android/ui/view/composables/SearchListItem.kt
@@ -1,11 +1,14 @@
 package com.example.examen2_android.ui.view.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -16,8 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.examen2_android.model.Show
 
 
@@ -32,10 +38,25 @@ fun SearchListItem(show: Show, modifier: Modifier = Modifier, onClick: (Show) ->
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(4.dp),
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth().padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val painter = rememberAsyncImagePainter(model = show.image?.original)
+            Image(
+                painter = painter,
+                contentDescription = "Show Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(100.dp)
+                    .aspectRatio(9 / 16f)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+            )
             Column(
                 modifier = Modifier
-                    .align(Alignment.BottomStart)
+                    .align(Alignment.CenterVertically)
                     .padding(8.dp)
                     .background(MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.8f))
                     .padding(8.dp)
@@ -47,19 +68,22 @@ fun SearchListItem(show: Show, modifier: Modifier = Modifier, onClick: (Show) ->
                     overflow = TextOverflow.Ellipsis,
                     color = MaterialTheme.colorScheme.onSurface
                 )
-                Text(
-                    text = show.genres.joinToString(", "),
-                    style = MaterialTheme.typography.bodyMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = MaterialTheme.colorScheme.onSurface
-                )
+                show.genres?.let {
+                    Text(
+                        text = it.joinToString(", "),
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
                 Text(
                     text = "Rating: ${show.rating?.average ?: "N/A"}",
                     style = MaterialTheme.typography.bodySmall,
                     color = MaterialTheme.colorScheme.onSurface
                 )
             }
+
         }
     }
 }
